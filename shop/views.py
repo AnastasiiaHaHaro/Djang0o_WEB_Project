38c94de1324e4ac4428528c9202855ea40027811
AnastasiiaHaHaro <nastyagagaro@gmail.com>
2024-07-11 17:54:15 -0600

    first

---
+++ shop/views.py
@@ -0,0 +1,22 @@
+
+from .models import Course
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+# Create your views here.
+
+def index(request):
+    courses = Course.objects.all()
+    return render(request, 'shop/course.html', {'courses': courses})
+
+def single_course(request, course_id):
+    # OPTION 1
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'shop/single_course.html', {'course': course})
+    # except Course.DoesNotExist:
+    #     raise Http404()
+
+    # OPTION 2
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'shop/single_course.html', {'course': course})
